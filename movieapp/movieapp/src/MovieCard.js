b21ba0f4dd185d88d2bf3e135002e2c58f5b4a95
Nikhil Pounikar <135841376+nikhilpounikar@users.noreply.github.com>
2023-07-22 13:41:22 +0530

    Update MovieCard.js
    Increase star rating and decrease function created

--- movieapp/movieapp/src/MovieCard.js
+++ movieapp/movieapp/src/MovieCard.js
@@ -6,14 +6,36 @@ export default class MovieCard extends Component {
   constructor(){
       super();
       this.state = {
-        title:"Avengers",
-        plot:"The best movie has ever created by marvel studies Starring Tony Stark",
+        title: "Avengers",
+        plot: "The best movie has ever been created by marvel studies Starring Tony Stark",
         price:19.99,
         ratings:9.8,
         stars:0,
       }
   }
-  
+
+  decreaseCount = ()=>{
+
+    if(this.state.stars==0)
+      return;
+    //set state method update field and re-renderes component
+    // setState() form1 
+    this.setState({
+      stars: this.state.stars + 0.5,
+    });
+  }
+  // direct calling function in onClick has a limitation it does not recognize state variable because it works on reference 
+  // which may be lost hence binding is required but the arrow function implicitly binds the function than writing this.increaseCount,bind(this)
+  increaseCount = ()=>{
+
+    if(this.state.stars==5)
+      return;
+    //set state method update field and re-rendered component
+    // setState() form2 
+    this.setState((prevState) => {
+      stars: prevState.stars + 0.5,
+    });
+  }
   render() {
     const {title,plot, price,ratings,stars} =  this.state;
     return (
@@ -31,11 +53,11 @@ export default class MovieCard extends Component {
                 <div className="rating">{ratings}</div>
                 <div className="star-dis">
                     //decrease image goes here
-                    <img alt="decrease" src="https://cdn-icons-png.flaticon.com/128/2801/2801932.png" className="str-btn" />
+                    <img onClick={this.decreaseCount} alt="decrease" src="https://cdn-icons-png.flaticon.com/128/2801/2801932.png" className="str-btn" />
                     // Star images goes here
                     <img alt="stars" src="https://cdn-icons-png.flaticon.com/128/2107/2107957.png" className="stars" />
                     // increase images goes here
-                    <img alt="increase" src="https://cdn-icons-png.flaticon.com/128/1828/1828925.png" className="str-btn" />
+                    <img alt="increase" onClick={this.increaseCount} src="https://cdn-icons-png.flaticon.com/128/1828/1828925.png" className="str-btn" />
                     //count goes here
                     <span className="starCount">{stars}</span>
                 </div>
