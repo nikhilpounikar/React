e34904f681c47aebe80333eda6d2c94675edcce3
Nikhil Pounikar <135841376+nikhilpounikar@users.noreply.github.com>
2023-07-22 13:31:20 +0530

    Update MovieCard.js
    state variable used

--- movieapp/movieapp/src/MovieCard.js
+++ movieapp/movieapp/src/MovieCard.js
@@ -24,11 +24,11 @@ export default class MovieCard extends Component {
               <img src="	https://cdn.marvel.com/content/1x/themarvels_lob_crd_04.jpg" alt=""></img>
             </div>
             <div className="right">
-              <div className="title">Avengers Infinity War</div>
-              <div className="plot">The best movie has ever created by marvel studies Starring Tony Stark</div>
-              <div className="price">$ 19.99</div>
+              <div className="title">{title}</div>
+              <div className="plot">{plot}</div>
+              <div className="price">$ {price}</div>
               <div className="footer">
-                <div className="rating">9.8</div>
+                <div className="rating">{ratings}</div>
                 <div className="star-dis">
                     //decrease image goes here
                     <img alt="decrease" src="https://cdn-icons-png.flaticon.com/128/2801/2801932.png" className="str-btn" />
@@ -37,7 +37,7 @@ export default class MovieCard extends Component {
                     // increase images goes here
                     <img alt="increase" src="https://cdn-icons-png.flaticon.com/128/1828/1828925.png" className="str-btn" />
                     //count goes here
-                    <span className="starCount">0</span>
+                    <span className="starCount">{stars}</span>
                 </div>
                 <button className="favourite-btn">Like</button>
                 <button className="cart-btn">Add to cart</button>
