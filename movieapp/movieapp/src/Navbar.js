c300096699ce9de16613c8817388fdea7dfcc938
nikhilpounikar <nikhilpounikar@gmail.com>
2023-07-28 17:48:15 +0530

    Styled component used

--- movieapp/movieapp/src/Navbar.js
+++ movieapp/movieapp/src/Navbar.js
@@ -1,31 +1,73 @@
-import { Component } from "react";
+import React from "react";
+//import './styles.css';
+import styled from "styled-components"
+
+const Nav =  styled.div`
+    height: 70px;
+	background: linear-gradient(170deg, #1bc059, #0d47a1);
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	position: relative;
+  
+`;
+
+const Title = styled.div`
+    font-size: 24px;
+    color: #fff;
+    font-weight: 600;
+    font-family: 'Times New Roman', Times, serif;
+    text-transform: uppercase;
+    margin-left: 20px ;
+`;
 
 
-class Navbar extends Component{
+const CartImg = styled.img`
+    height: 48px;
+    margin-right: 20px;
+`;
 
-    render(){
-        return (
-            <>
-                {/* Using block level style */}
-                {/* <div style={styles.nav}>
-                   <div>
-                    Title
-                   </div>
-                   <div>
-                    <img alt="cart_icon"/>
-                        <span>0</span>
-                    
-                    </div>
-                </div> */}
-            </>
-        )
+const CartIconContainer = styled.div`
+    position: relative;
+    cursor: pointer;
+`;
+
+const CartCount = styled.div`
+    background: yellow;
+    border-radius: 50%;
+    padding: 4px 8px;
+    position: absolute;
+    right: 10px;
+    top: -5px;
+    font-size: 12px;
+`;
+
+
+
+class Navbar extends React.Component {
+
+  
+    render() {   
+       
+      return (
+      <>
+      <Nav>
+        
+      <Title>MOVIE APP</Title>
+        
+        <CartIconContainer>
+            <CartImg alt="Cart-Icon" 
+                 src = "https://cdn-icons-png.flaticon.com/128/891/891462.png"    
+            />
+            <CartCount >5</CartCount>
+        </CartIconContainer>
+      </Nav>
+
+      </>
+      )
     }
-
-}
-
-
-export default Navbar;
-
-const styles = {
-    nav : { width:"100%",height:70,backgroundColor:"#db2828",display:"flex",justifyContent:"space-around",color:'white'}
-}
+  }  
+ 
+  
+  export default Navbar;
+  
